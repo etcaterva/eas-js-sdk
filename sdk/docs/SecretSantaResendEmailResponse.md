e5c714418398b23729e2b5f224d5ac810c45395f
David Naranjo <dnaranjo89@gmail.com>
2024-08-07 18:34:10 +0200

    Generate TS sdk

--- sdk/docs/SecretSantaResendEmailResponse.md
+++
@@ -1,9 +0,0 @@
-# EchaloasuerteJsSdk.SecretSantaResendEmailResponse
-
-## Properties
-
-Name | Type | Description | Notes
------------- | ------------- | ------------- | -------------
-**newResult** | **String** |  | [optional] 
-
-
---
+++ sdk/src/apis/index.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CoinApi';
+export * from './GroupsApi';
+export * from './InstagramApi';
+export * from './LetterApi';
+export * from './LinkApi';
+export * from './LotteryApi';
+export * from './PaypalApi';
+export * from './RaffleApi';
+export * from './RandomNumberApi';
+export * from './SecretSantaApi';
+export * from './ShiftsApi';
+export * from './SpinnerApi';
+export * from './TiktokApi';
+export * from './TournamentApi';
